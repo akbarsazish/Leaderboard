8371107d731fa94169920670289c55f98b3bb7f6
akbrsazish <sazishaliakbar@gmail.com>
2023-07-07 22:19:52 +0330

    removed the style.css file form index.html

---
+++ src/modules/showScore.js
@@ -0,0 +1,24 @@
+const showScore = (gameId) => {
+    const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/wUcdYlqfnVytiFkqhyG8/scores/`;
+  
+    fetch(url, {
+      method: 'GET',
+    })
+      .then((data) => data.json())
+      .then((response) => {
+  
+        const scoreList = document.getElementById('scoreList');
+        scoreList.innerHTML = '';
+        response.result.forEach((score) => {
+          const li = document.createElement('li');
+          li.textContent = `User: ${score.user}, Score: ${score.score}`;
+          scoreList.appendChild(li);
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+        // Handle any errors that occur during the process
+      });
+  };
+
+export {showScore}
