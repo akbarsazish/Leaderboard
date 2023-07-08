6e19d1fb8d512cab9f776e31ad95c6367805b216
akbrsazish <sazishaliakbar@gmail.com>
2023-07-08 13:12:46 +0330

    add a function while refreshing the page it update the list

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import './style.css';
 import createGame from './modules/createGame.js';
 import showScore from './modules/showScore.js';
-import handleSubmit from './modules/submitGame';
+import handleSubmit from './modules/submitGame.js';
 /* eslint-disable */
 window.load = createGame();
 handleSubmit();
 window.load = showScore();
 
 const refreshButton = document.getElementById('refresh-button');
-refreshButton.addEventListener('click', () => {
-  location.reload()
+const scoreList = document.getElementById('scoreList');
+
+refreshButton.addEventListener('click', async () => {
+  try {
+    const gameId = 'wUcdYlqfnVytiFkqhyG8';
+    const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    scoreList.innerHTML = '';
+    data.result.forEach((score) => {
+      const li = document.createElement('li');
+      li.textContent = `User: ${score.user}, Score: ${score.score}`;
+      scoreList.appendChild(li);
+      location.reload()
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
